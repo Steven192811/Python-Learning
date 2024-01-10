4e9cb849a4cefa5a6350331631f43f8258421432
Steven Marquez <steven192811@gmail.com>
2024-01-10 17:17:22 +0100

    Finished lecture on introduction to strings

---
+++ Python-Object-And-Data-Structures_Basics/introduction_to_strings.py
@@ -0,0 +1,17 @@
+"hello"
+"world"
+"this is also a string"
+"I'm going on a run"
+print("hello world one")
+print("hello world two")
+print("hello \nworld")
+print("hello \tworld")
+print(len("hello"))
+print(len("I am"))
+mystring = "Hello World"
+print(mystring)
+print(mystring[0])
+print(mystring[8])
+print(mystring[-2])
+print(mystring[-3])
+print(mystring[-4])
