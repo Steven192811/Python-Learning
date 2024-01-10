93bf35159bb8b814c042351f1d58227829260803
Steven Marquez <steven192811@gmail.com>
2024-01-10 17:49:42 +0100

    Fiinished lecture on immutability

---
+++ Python-Object-And-Data-Structures_Basics/immutability.py
@@ -0,0 +1,19 @@
+name ="Sam"
+name[0] = "P" # TypeError: 'str' object does not support item assignment
+last_letters = name[1:]
+print (last_letters)
+print ("P" + last_letters)
+x = "Hello World"
+x = x + " it is beautiful outside!"
+print (x)
+letter = 'z'
+print (letter * 10)
+print (2 + 3)
+print ('2' + '3')
+x = 'Hello World'
+print (x.upper())
+print (x.lower())
+print (x.split())
+x = 'Hi this is a string'
+print (x.split())
+print (x.split('i'))
