c8f87538ee9dfdd4c8dbf2d7535ce01d6106d5a3
Steven Marquez <steven192811@gmail.com>
2024-01-12 19:54:28 +0100

    Finished lecture on Booleans in Python

---
+++ Python-Object-And-Data-Structures_Basics/booleans_in_python.py
@@ -0,0 +1,7 @@
+True
+False # Note: Capital T and F
+type(False) # booleans are a data type
+print (1 > 2) # False
+print (1 == 1) # True
+b = None # None is a placeholder for an object that we don't want to reassign yet
+print(b) # None
