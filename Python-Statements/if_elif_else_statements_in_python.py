a06ab880de89b7cedac205e0e7029f75779525a9
Steven Marquez <steven192811@gmail.com>
2024-01-15 17:15:20 +0100

    Finished lecture on if / elif / else Statements in Python

---
+++ Python-Statements/if_elif_else_statements_in_python.py
@@ -0,0 +1,27 @@
+if True:
+    print("True") # True
+
+hungry = True
+if hungry:
+    print("Feed me!")
+else:
+    print("I'm not hungry")
+
+loc = 'Store' # 'Auto Shop', 'Bank', 'Store
+
+if loc == 'Auto Shop':
+    print("Cars are cool!")
+elif loc == 'Bank':
+    print("Money is cool!")
+elif loc == 'Store':
+    print("Welcome to the store!")
+else:
+    print("I do not know much.")
+
+name = 'Steven'
+if name == 'Frankie':
+    print("Hello Frankie!")
+elif name == 'Sammy':
+    print("Hello Sammy!")
+else:
+    print("What is your name?")
