97899c0c54c2f97afd765e78cdd789ceccce602e
Steven Marquez <steven192811@gmail.com>
2024-01-16 22:12:42 +0100

    Passed 2nd question on Functions Test in Python

--- 04-Python-Methods-And-Functions/functions_test.py
+++ 04-Python-Methods-And-Functions/functions_test.py
@@ -4,3 +4,10 @@
 def myfunc():
     print('Hello World')
 
+#2: print Hello Name
+#Write a function called myfunc that takes in a name and prints 'Hello Name'
+
+def myfunc(Name):
+    print(f'Hello {Name}')
+
+
