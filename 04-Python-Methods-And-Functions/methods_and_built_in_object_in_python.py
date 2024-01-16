89eb676388ae6609e3c36efd4c1afc329c124867
Steven Marquez <steven192811@gmail.com>
2024-01-16 17:29:57 +0100

    Finished Lecture on Methods and built in Objects in Python

---
+++ 04-Python-Methods-And-Functions/methods_and_built_in_object_in_python.py
@@ -0,0 +1,13 @@
+#Methods and the Python Documentation
+
+#Methods are essentially functions built into objects.
+#Methods perform specific actions on an object and can also take arguments, just like a function.
+#Methods are in the form: object.method(arg1,arg2,etc...)
+
+mylist = [1,2,3]
+mylist.append(4) #.append() is a method
+print(mylist) # [1, 2, 3, 4]
+
+mylist.pop() #.pop() is a method
+print(mylist) # [1, 2, 3]
+
