c1a3081289f71389015c9f94bb3ec042947dfbe9
Steven Marquez <steven192811@gmail.com>
2024-01-16 17:36:28 +0100

    Finished Lecture on Introduction to Functions in Python

---
+++ 04-Python-Methods-And-Functions/introduction_to_functions_in_python.py
@@ -0,0 +1,3 @@
+# Functions in Python
+# def name_of_function(): #just like in Ruby, def is used to define a function
+# this is pretty much an introduction to Object Oriented Programming (OOP) in Python
