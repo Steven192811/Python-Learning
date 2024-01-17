f684514262eb565943a93bef56b71d3f00199d57
Steven Marquez <steven192811@gmail.com>
2024-01-17 14:03:39 +0100

    Passed 4rd question on Functions Test in Python

--- 04-Python-Methods-And-Functions/functions_test.py
+++ 04-Python-Methods-And-Functions/functions_test.py
@@ -20,3 +20,12 @@ def myfunc(boolean):
     else:
         return 'Goodbye'
 
+#4: Using Booleans
+#Write a function called myfunc that takes three arguments, x, y, and z.
+# If z is True, return x. If z is False, return y.
+
+def myfunc(x,y,z):
+    if z == True:
+        return x
+    else:
+        return y
