8ab3e041b8a9903a740c8b2c13a89f8936e2312b
Steven Marquez <steven192811@gmail.com>
2024-01-17 16:10:55 +0100

    Passed 7th question on Functions Test in Python

--- 04-Python-Methods-And-Functions/functions_test.py
+++ 04-Python-Methods-And-Functions/functions_test.py
@@ -47,3 +47,15 @@ def is_even(x):
         return True
     else:
         return False
+
+
+#7: is_greater
+#Write a function called is_greater that takes in two arguments,
+# and returns True if the first value is greater than the second,
+# False if it is less than or equal to the second.
+
+def is_greater(x,y):
+    if x > y:
+        return True
+    else:
+        return False
