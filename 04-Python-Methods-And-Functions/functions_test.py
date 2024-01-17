cf7446288f985ace643d19c2e4e225023668ab13
Steven Marquez <steven192811@gmail.com>
2024-01-17 16:08:50 +0100

    Passed 6th question on Functions Test in Python

--- 04-Python-Methods-And-Functions/functions_test.py
+++ 04-Python-Methods-And-Functions/functions_test.py
@@ -37,3 +37,13 @@ def myfunc(x,y,z):
 def myfunc(x,y):
     return x+y
 
+
+#6: is_even
+#Write a function called is_even that takes in one argument,
+# and returns True if the passed-in value is even, False if it is not.
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
