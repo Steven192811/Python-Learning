bf5c4ef61772178def843edd774f493946434477
Steven Marquez <steven192811@gmail.com>
2024-01-19 19:43:00 +0100

    Completed level 2 solution #2 Function Practice problem in Python

--- 04-Python-Methods-And-Functions/level_2_practice.py
+++ 04-Python-Methods-And-Functions/level_2_practice.py
@@ -1,5 +1,6 @@
 # Level 2
 
+#1
 # Given a list of ints, return True if the array contains a 3 next to a 3
 # somewhere
 
@@ -11,3 +12,14 @@ def has_33(nums):
         return True
 
  return False
+
+
+#2
+# Paper Doll: Given a string, return a string where for every character in the
+# original there are three characters
+
+def paper_doll(text):
+    result = '' # create an empty string
+    for char in text: # for every character in the text string
+        result += char * 3 # add the character 3 times to the result string
+    return result
