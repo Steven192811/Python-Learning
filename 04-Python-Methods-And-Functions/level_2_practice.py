e1be28d102fe655ebd40f164be2467be4f505f3d
Steven Marquez <steven192811@gmail.com>
2024-01-19 19:52:34 +0100

    Completed level 2 solution #3 Function Practice problem in Python

--- 04-Python-Methods-And-Functions/level_2_practice.py
+++ 04-Python-Methods-And-Functions/level_2_practice.py
@@ -23,3 +23,18 @@ def paper_doll(text):
     for char in text: # for every character in the text string
         result += char * 3 # add the character 3 times to the result string
     return result
+
+
+#3
+# Blackjack: Given three integers between 1 and 11, if their sum is less than
+# or equal to 21, return their sum. If their sum exceeds 21 and there's an
+# eleven, reduce the total sum by 10. Finally, if the sum (even after
+# adjustment) exceeds 21, return 'BUST'
+
+def blackjack(a,b,c):
+    if sum((a,b,c)) <= 21: # if the sum of a, b, and c is less than or equal to 21
+        return sum((a,b,c)) # return the sum of a, b, and c
+    elif sum((a,b,c)) <= 31 and 11 in (a,b,c): # if the sum of a, b, and c is less than or equal to 31 and 11 is in a, b, or c
+        return sum((a,b,c)) - 10 # return the sum of a, b, and c minus 10
+    else:
+        return 'BUST' # return 'BUST'
