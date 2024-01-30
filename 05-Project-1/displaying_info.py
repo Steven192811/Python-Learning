3d178bcb2c29ae31cf68cfc1a4fe91d967482c86
Steven Marquez <steven192811@gmail.com>
2024-01-30 11:56:31 +0100

    Finished Lecture on Accepting user input in Python

--- 05-Project-1/displaying_info.py
+++ 05-Project-1/displaying_info.py
@@ -12,3 +12,29 @@ display(row1, row2, row3)
 row2[1] = "X"
 
 display(row1, row2, row3)
+
+input("Please enter a value: ")
+
+result = input("Please enter a value: ")
+
+type(result) # str
+
+result = int(input("Please enter a value: "))
+
+result_int = int(result)
+
+type(result_int) # int
+
+type(2.3) # float
+
+float("3.14") # 3.14
+
+position_index = int(input("Choose an index position: "))
+
+row1[position_index]
+
+row2[position_index]
+
+result = input("Enter a number here: ")
+
+input("Enter a number here: ") 
