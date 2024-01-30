a739cb6506121923d808e87c1b52047ec7da103e
Steven Marquez <steven192811@gmail.com>
2024-01-30 09:14:12 +0100

    Passed 1st question on Methods and functions homework in Python

---
+++ 04-Python-Methods-And-Functions/methods_and_functions_homework.py
@@ -0,0 +1,12 @@
+# Functions and methods homework
+
+# Write a function that computes the volume of a sphere given its radius.
+# The volume of a sphere is given as:
+# 4/3 * pi * radius ** 3
+
+def vol(rad):
+    return (4/3) * (3.14) * (rad ** 3)
+
+print(vol(2)) # 33.49333333333333
+print(vol(3)) # 113.03999999999999
+
