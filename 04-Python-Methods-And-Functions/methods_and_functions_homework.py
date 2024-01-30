b4b9a1d35f30b56fd9ac3941b1d500f62821a080
Steven Marquez <steven192811@gmail.com>
2024-01-30 09:17:11 +0100

    Passed 2nd question on Methods and functions homework in Python

--- 04-Python-Methods-And-Functions/methods_and_functions_homework.py
+++ 04-Python-Methods-And-Functions/methods_and_functions_homework.py
@@ -10,3 +10,20 @@ def vol(rad):
 print(vol(2)) # 33.49333333333333
 print(vol(3)) # 113.03999999999999
 
+# Write a function that checks whether a number is in a given range (inclusive of high and low)
+
+def ran_check(num,low,high):
+    if num in range(low,high+1):
+        print(f'{num} is in the range between {low} and {high}')
+    else:
+        print(f'{num} is not in the range between {low} and {high}')
+
+ran_check(5,2,7) # 5 is in the range between 2 and 7
+
+# If you only wanted to return a boolean:
+
+def ran_bool(num,low,high):
+    return num in range(low,high+1)
+
+print(ran_bool(3,1,10)) # True
+
