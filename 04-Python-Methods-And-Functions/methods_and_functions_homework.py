da8478dd5ec525bf3b1f54512b9e4a54604a60e6
Steven Marquez <steven192811@gmail.com>
2024-01-30 09:29:41 +0100

    Passed 5th question on Methods and functions homework in Python

--- 04-Python-Methods-And-Functions/methods_and_functions_homework.py
+++ 04-Python-Methods-And-Functions/methods_and_functions_homework.py
@@ -64,3 +64,19 @@ def unique_list(lst):
 
 
 print(unique_list([1,1,1,1,2,2,3,3,3,3,4,5])) # [1, 2, 3, 4, 5]
+
+# Write a Python function to multiply all the numbers in a list.
+
+# Sample List : [1, 2, 3, -4]
+
+# Expected Output : -24
+
+def multiply(numbers):
+    total = 1
+    for num in numbers:
+        total *= num
+    return total
+
+print(multiply([1,2,3,-4])) # -24
+print(multiply([1,2,3,-4,10])) # -240
+
