a5fee95796e07437d346d315b4cf4fd4c3bfb343
Steven Marquez <steven192811@gmail.com>
2024-01-31 23:09:03 +0100

    Finished Lecture on Validating User Input in Python

--- 05-Project-1/displaying_info.py
+++ 05-Project-1/displaying_info.py
@@ -1,4 +1,4 @@
-# Displaying Information
+                        # Displaying Information
 def display(row1, row2, row3):
     print(row1)
     print(row2)
@@ -14,7 +14,7 @@ row2[1] = "X"
 
 display(row1, row2, row3)
 
-# Accepting User Input
+                        # Accepting User Input
 input("Please enter a value: ")
 
 result = input("Please enter a value: ")
@@ -41,4 +41,48 @@ result = input("Enter a number here: ")
 
 input("Enter a number here: ")
 
-# Validating User Input
+                        # Validating User Input
+def user_choice():
+
+# VARIABLES
+
+    # Initial
+    choice = "WRONG"
+    acceptable_range = range(0, 10)
+    within_range = False
+
+# Two Conditions to Check
+# Digit or Within_Range == False
+
+    while choice.isdigit() == False or within_range == False:
+
+        choice = input("Please enter a number (0-10): ")
+
+# DIGIT CHECK
+        if choice.isdigit() == False:
+            print("Sorry that is not a digit!")
+
+# RANGE CHECK
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print("Sorry, you are out of acceptable range (0-10)")
+                within_range = False
+
+    return int(choice)
+
+user_choice() # 5
+
+some_value = "100"
+
+some_value.isdigit() # True
+
+result = "WRONG VALUE"
+
+acceptable_values = [0, 1, 2]
+
+result in acceptable_values # False
+
+result not in acceptable_values # True
+
