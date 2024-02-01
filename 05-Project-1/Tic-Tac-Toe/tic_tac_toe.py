a6937539be2f71d7e698e918c3eafbc1f26d7f8d
Steven Marquez <steven192811@gmail.com>
2024-02-01 14:18:32 +0100

    added method to choose a random position

--- 05-Project-1/Tic-Tac-Toe/tic_tac_toe.py
+++ 05-Project-1/Tic-Tac-Toe/tic_tac_toe.py
@@ -2,17 +2,18 @@ from IPython.display import clear_output
 
 def display_board(board):
     clear_output()
-    print(board[7] + "|" + board[8] + "|" + board[9])
+    print(board[7] + "|" + board[8] + "|" + board[9]) # 7|8|9
     print("-|-|-")
-    print(board[4] + "|" + board[5] + "|" + board[6])
+    print(board[4] + "|" + board[5] + "|" + board[6]) # 4|5|6
     print("-|-|-")
-    print(board[1] + "|" + board[2] + "|" + board[3])
+    print(board[1] + "|" + board[2] + "|" + board[3]) # 1|2|3
 
 
 def player_input():
-    marker = ""
+    marker = "" # initialize marker
     while marker != "X" and marker != "O":
-        marker = input("Player 1, choose X or O: ").upper()
+        marker = input("Player 1, choose X or O: ").upper() # ask player 1 to choose X or O
+
     if marker == "X":
         return ("X", "O")
     else:
@@ -20,10 +21,10 @@ def player_input():
 
 
 def place_marker(board, marker, position):
-    board[position] = marker
+    board[position] = marker # place marker at position on board
 
 
-def win_check(board, mark):
+def win_check(board, mark): # check if mark has won
     return ((board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
             (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
             (board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
@@ -33,3 +34,15 @@ def win_check(board, mark):
             (board[1] == mark and board[5] == mark and board[9] == mark) or  # diagonal
             (board[3] == mark and board[5] == mark and board[7] == mark))  # diagonal
 
+
+import random
+
+
+def choose_first():
+
+    flip = random.randint(0, 1) # generate random number between 0 and 1
+
+    if flip == 0:
+        return "Player 1"
+    else:
+        return "Player 2"
