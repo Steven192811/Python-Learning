9d4a6251fc95f7804d5cd615e5143e729d2b8148
Steven Marquez <steven192811@gmail.com>
2024-02-01 13:38:47 +0100

    Added method for the player to choose between X or O

--- 05-Project-1/Tic-Tac-Toe/tic_tac_toe.py
+++ 05-Project-1/Tic-Tac-Toe/tic_tac_toe.py
@@ -9,3 +9,13 @@ def display_board(board):
     print(board[1] + "|" + board[2] + "|" + board[3])
 
 
+def player_input():
+    marker = ""
+    while marker != "X" and marker != "O":
+        marker = input("Player 1, choose X or O: ").upper()
+    if marker == "X":
+        return ("X", "O")
+    else:
+        return ("O", "X")
+
+
