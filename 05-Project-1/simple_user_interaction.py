092ce972425fed427451447e75856d42dda25627
Steven Marquez <steven192811@gmail.com>
2024-02-01 13:24:39 +0100

    Completed the position choice function for a simple user interaction

--- 05-Project-1/simple_user_interaction.py
+++ 05-Project-1/simple_user_interaction.py
@@ -8,3 +8,14 @@ def display_game(game_list):
 
 display_game(game_list)
 
+def position_choice():
+    choice = "wrong"
+
+    while choice not in ["0", "1", "2"]:
+        choice = input("Pick a position (0, 1, 2): ")
+
+        if choice not in ["0", "1", "2"]:
+            print("Sorry, invalid choice!")
+
+    return int(choice)
+
