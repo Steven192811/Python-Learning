c96949a38a387acb03624330f32507c946a2d672
Steven Marquez <steven192811@gmail.com>
2024-02-02 19:24:37 +0100

    Finished Lecture on Pip install and PyPi in Python

---
+++ 07-Modules-and-Packeges/Pip install PyPi.py
@@ -0,0 +1,11 @@
+# pip is a package manager for Python that allows you to install Python packages.
+# You can use pip to install packages from the Python Package Index and other indexes.
+# pip is a recursive acronym that can stand for either "Pip Installs Packages" or "Pip Installs Python".
+
+# To install a package from PyPi, you can use the following command:
+# pip install package_name
+
+# For example, to install the requests package, you can use the following command:
+# pip install requests
+
+# pip is like npm in Node.js, or Gem in Ruby.
