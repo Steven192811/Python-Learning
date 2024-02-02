cb9489fb421059e99fefe8379a6bf6835ae85211
Steven Marquez <steven192811@gmail.com>
2024-02-02 15:31:41 +0100

    completed Lecture on Attributes and Class Keywords in Python

---
+++ 06-OOP/attributes_and_class_keyword.py
@@ -0,0 +1,20 @@
+my_list = [1, 2, 3]
+print(my_list)
+
+myset = set()
+
+type(myset)
+
+class Dog(): # class keyword
+
+    def __init__(self, breed, name, spots):
+
+        # Attributes
+        # We take in the argument
+        # Assign it using self.attribute_name
+        self.breed = breed
+        self.name = name
+        self.spots = spots
+
+my_dog = Dog(mybreed='Puddle', name='Niño', spots=False)
+print(my_dog) 
