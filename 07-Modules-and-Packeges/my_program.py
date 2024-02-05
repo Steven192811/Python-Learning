94a335ed6eac0607058123298c56a2113a5e02cb
Steven Marquez <steven192811@gmail.com>
2024-02-05 20:07:22 +0100

    Changed naming convention of folders

--- 07-Modules-and-Packeges/my_program.py
+++ 07-Modules-and-Packeges/my_program.py
@@ -3,3 +3,6 @@ from my_main_package import some_main_script
 from my_main_package.sub_package import my_sub_script # This is how you import a sub-package
 
 my_func()
+
+some_main_script.report_main()
+my_sub_script.sub_report()
