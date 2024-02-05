ad96f69c053022b185145404b86d6bca964ed08c
Steven Marquez <steven192811@gmail.com>
2024-02-05 20:07:54 +0100

    Added comments

--- 07-Modules-and-Packeges/my_program.py
+++ 07-Modules-and-Packeges/my_program.py
@@ -1,8 +1,8 @@
 from modules_and_packages import my_func
-from my_main_package import some_main_script
+from my_main_package import some_main_script # This is how you import a module
 from my_main_package.sub_package import my_sub_script # This is how you import a sub-package
 
 my_func()
 
-some_main_script.report_main()
+some_main_script.report_main() 
 my_sub_script.sub_report()
