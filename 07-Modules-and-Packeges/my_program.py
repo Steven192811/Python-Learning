4bfb0a038d3f2b13e76bc55fd0a0b0ea95965e11
Steven Marquez <steven192811@gmail.com>
2024-02-05 20:00:17 +0100

    How to import from a sub folder

--- 07-Modules-and-Packeges/my_program.py
+++ 07-Modules-and-Packeges/my_program.py
@@ -1,4 +1,5 @@
 from modules_and_packages import my_func
 from my_main_package import some_main_script
+from my_main_package.sub_package import my_sub_script # This is how you import a sub-package
 
 my_func()
