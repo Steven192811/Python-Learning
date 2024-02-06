e2f7d19ec1874f8af55cd2dfb9d3ff58005ab33d
Steven Marquez <steven192811@gmail.com>
2024-02-06 22:37:11 +0100

    Fixes

--- 07-Modules-and-Packeges/my_program.py
+++ 07-Modules-and-Packeges/my_program.py
@@ -5,4 +5,4 @@ from my_main_package.sub_package import my_sub_script # This is how you import a
 my_func()
 
 some_main_script.report_main() # This is how you call a function from a module
-my_sub_script.sub_report()
+my_sub_script.sub_report() # This is how you call a function from a sub-package
