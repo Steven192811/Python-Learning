e3a8957fccb2ec3eee2aa8d17653bd94520df96d
Steven Marquez <steven192811@gmail.com>
2024-02-19 19:25:34 +0100

    added logic for second file

---
+++ 07-Modules-and-Packeges/_name_and_main_/two.py
@@ -0,0 +1,10 @@
+import first
+
+print ("TOP LEVEL in two.py")
+
+first.func()
+
+if __name__ == "__main__": # if this file is being run directly
+    print("two.py is being run directly")
+else:
+    print("two.py has been imported")
